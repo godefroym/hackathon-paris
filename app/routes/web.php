a2b7fb6d9ea41c31f2d9c17a7368018940180ac1
barbapapazes <e.soubiran25@gmail.com>
2026-02-28 21:22:36 +0100

    fix(app): remove useless content

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'index')->name('home');
 Route::inertia('/overlays/fact-check', 'overlays/fact-check/index')
     ->name('overlays.fact-check.index');
